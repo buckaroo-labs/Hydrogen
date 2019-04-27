059bbfb316238eb65fce5fb754f96d2fabb69e4c
Kent Heiner <kent_heiner@byu.net>
2019-04-27 10:18:19 -0700

    commit before branch

--- pgTemplate.php
+++ pgTemplate.php
@@ -23,6 +23,8 @@ require_once 'settings.php';
 <head>
 <link rel="stylesheet" type="text/css" href="Hydrogen/style.css">
 <meta http-equiv="CONTENT-TYPE" content="text/html; charset=windows-1252">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <?php  if (isset($pagetitle)) {
 echo "<title>" . $pagetitle . "</title>";
 //to prevent unintentionally setting the same title on subsequent pages using this template
