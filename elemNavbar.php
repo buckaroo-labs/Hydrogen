ee3adb3a6726ada3c61bc8a8c0867faf1fbdc988
Kent Heiner <46908086+ke7ijo@users.noreply.github.com>
2019-04-27 12:56:29 -0700

    Rwd (#1)
    * Update for responsive web design
    
    * Fix issues with login and search forms
    
    * update usage
    
    * Create Home link

---
+++ elemNavbar.php
@@ -0,0 +1,15 @@
+<!-- Navbar -->
+<div class="w3-top">
+  <div class="w3-bar w3-theme w3-top w3-left-align w3-large">
+    <a class="w3-bar-item w3-button w3-right w3-hide-large w3-hover-white w3-large w3-theme-l1" href="javascript:void(0)" onclick="w3_open()"><i class="fa fa-bars"></i></a>
+    <a href="#" class="w3-bar-item w3-button w3-theme-l1">Logo</a>
+    <a href="./" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Home</a>
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-hover-white">About</a>
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-hover-white">News</a>
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Contact</a>
+	<?php 
+		include('Hydrogen/elemSearchForm.php');  
+		include('Hydrogen/elemLoginStatusbar.php'); 
+	?>
+  </div>
+</div>
