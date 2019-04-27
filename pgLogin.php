2513be289fae9be9a476a98cf93c04f671c49605
Kent Heiner <kent_heiner@byu.net>
2019-04-27 12:48:53 -0700

    update usage

--- pgLogin.php
+++ pgLogin.php
@@ -5,10 +5,25 @@
 EXAMPLE USAGE:
 <?php 
 $pagetitle="Log In | MySite";
-$headline = '<h1>My Site</h1><h3>Please log in</h3>' ;
 include "Hydrogen/pgTemplate.php";
-include "Hydrogen/pgLogin.php"; 
 ?>
+
+<!-- Main content: shift it to the right by 250 pixels when the sidebar is visible -->
+<div class="w3-main" style="margin-left:250px">
+
+  <div class="w3-row w3-padding-64">
+    <div class="w3-twothird w3-container">
+
+	<?php include "Hydrogen/pgLogin.php"; ?>
+
+    </div>
+    <div class="w3-third w3-container">
+    </div>
+  </div>
+
+</div>
+
+<?php include "Hydrogen/elemFooter.php"; ?>
 </body></html>
 
 ******************************************************/
