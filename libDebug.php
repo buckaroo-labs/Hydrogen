8fdc1c5e0d754950b769e74f1f485f4a259e14bb
Kent Heiner <46908086+ke7ijo@users.noreply.github.com>
2019-06-23 09:07:10 -0700

    Update for command-line usage

--- libDebug.php
+++ libDebug.php
@@ -10,10 +10,10 @@ function debug($info){
 	if ($settings['DEBUG']) {
 
 		$output[0]=date("Y-m-d H:i:s");
-		$output[1]=$_SERVER['REMOTE_ADDR'];
+		if (isset($_SERVER['REMOTE_ADDR'])) $output[1]=$_SERVER['REMOTE_ADDR']; else $output[1]="command line";
 		if (isset($_SESSION['username'])) $output[2]=$_SESSION['username']; else $output[2]='null';
 		$output[3]=$info;
-		$output[4]=$_SERVER['REQUEST_URI'];
+		if (isset($_SERVER['REQUEST_URI'])) $output[4]=$_SERVER['REQUEST_URI']; else $output[4]=$_SERVER['SCRIPT_FILENAME'];
 		$fp = fopen($settings['DEBUG_PATH'], 'a');
 		fwrite($fp, implode("\t | ",$output) . "\n");
 		fclose($fp);
