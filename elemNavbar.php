df920acb16674b5443e669190a4139385d5ef460
buckaroo-labs <46908086+buckaroo-labs@users.noreply.github.com>
2023-12-26 14:04:19 -0800

    Update elemNavbar.php
    color customization

--- elemNavbar.php
+++ elemNavbar.php
@@ -1,21 +1,25 @@
 <?php 
+if (!isset($settings['color1']))  $settings['color1']="w3-green";
+if (!isset($settings['color2']))  $settings['color2']="w3-blue";
+if (!isset($settings['color3']))  $settings['color2']="w3-hover-white";
+
 if (!isset($navbar_links)) {
 	//These are the (demo) defaults. Override them in an included settings file and/or page template.
 
 	$navbar_links[0]=array("name"=>'Logo',"href"=>"#","class"=>"w3-theme-l1");
 	//Put your log image here below in place of "Logo" text above
 	//$navbar_links[0]=array("name"=>'<img src="images/logo_thumbnail.jpg">',"href"=>"#","class"=>"w3-theme-l1");
-	$navbar_links[1]=array("name"=>"Home","href"=>"./","class"=>"w3-hide-small w3-hover-white");
-	$navbar_links[2]=array("name"=>"About","href"=>"#","class"=>"w3-hide-small w3-hover-white");
-	$navbar_links[3]=array("name"=>"News","href"=>"#","class"=>"w3-hide-small w3-hover-white");
-	$navbar_links[4]=array("name"=>"Contact","href"=>"#","class"=>"w3-hide-small w3-hover-white");
+	$navbar_links[1]=array("name"=>"Home","href"=>"./","class"=>"w3-hide-small " . $settings['color3']);
+	$navbar_links[2]=array("name"=>"About","href"=>"#","class"=>"w3-hide-small " . $settings['color3']);
+	$navbar_links[3]=array("name"=>"News","href"=>"#","class"=>"w3-hide-small " . $settings['color3']);
+	$navbar_links[4]=array("name"=>"Contact","href"=>"#","class"=>"w3-hide-small " . $settings['color3'] );
 }
 ?>
 
 <!-- Navbar -->
 <div class="w3-top">
-  <div class="w3-bar w3-green w3-top w3-left-align w3-large">
-    <a class="w3-bar-item w3-button w3-right w3-hide-large w3-hover-white w3-large w3-green" href="javascript:void(0)" onclick="w3_open()"><i class="fa fa-bars"></i></a>
+  <div class="w3-bar  <?php echo $settings['color1'] ?> w3-top w3-left-align w3-large">
+    <a class="w3-bar-item w3-button w3-right w3-hide-large w3-hover-white w3-large  <?php echo $settings['color1'] ?>" href="javascript:void(0)" onclick="w3_open()"><i class="fa fa-bars"></i></a>
 <?php 
 
 	$arrlength=count($navbar_links);
