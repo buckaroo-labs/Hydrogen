ea977d65be08b6cafa52e3d79c5da4569096bd36
Kent Heiner <kent_heiner@byu.net>
2026-01-07 06:48:39 -0800

    A/B tests

--- template/settingsHydrogen.php
+++ template/settingsHydrogen.php
@@ -13,12 +13,26 @@ $settings['DEFAULT_DB_MAXRECS'] = 150;
 @include ("settingsPasswords.php");
 
 //If these color settings are not set here, defaults may be assigned elsewhere in the code.
+//These aren't colors per se, but classes assigned to elements, which will be colored
+//according to w3.css specs. You can use the w3 color classes or something else, or even 
+//override the colors of the w3 classes in your styles.css file.
 $settings['color1']="w3-blue";
 $settings['color2']="w3-green";
 //button hover colors
 $settings['color3']="w3-hover-white";
 $settings['color4']="w3-hover-blue";
 $settings['color5']="w3-hover-black";
+
+// A/B Testing:
+$abTest=false;
+if ($abTest) {
+	$settings['color1']="w3-red";
+	$settings['color2']="w3-black";
+
+	$settings['color3']="w3-hover-black";
+	$settings['color4']="w3-hover-red";
+	$settings['color5']="w3-hover-white";
+}
 
 
 $logo_image="logo.png";
@@ -38,8 +52,10 @@ if (!isset($_GET['menu'])) {
     $sidebar_links[sizeof($sidebar_links)]=array("name"=>'Page 3',"href"=>"3.php","class"=>$settings['color4']);
     $sidebar_links[sizeof($sidebar_links)]=array("name"=>'Page 4',"href"=>"4.php","class"=>$settings['color4']);
 }
+
 //For testing:
 //unset ($sidebar_links);
+
 $active_menu_class="w3-hide-small " . $settings['color5'];
 $other_menu_class="w3-hide-small " . $settings['color5'];
 $navbar_links[sizeof($navbar_links)]=array("name"=>'<img src="logo.png" height="20">',"href"=>"index.php","class"=>"w3-theme-l2");
