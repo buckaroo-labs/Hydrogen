5f72abe42bb615efa1cdaf514c179e64003e2bfd
buckaroo-labs <heiner@monstro.us>
2026-02-22 16:03:10 -0800

    avoid warnings

--- elements/LogoHeadline.php
+++ elements/LogoHeadline.php
@@ -1,13 +1,16 @@
 <div class="w3-row w3-padding-24">
 
 <?php 
-	if (isset($headline)  && (!isset($_GET['layout']))|| $_GET['layout']!='iframe') {
+	if (array_key_exists('layout',$_GET) && $_GET['layout']=='iframe') {
+		//do nothing
+	}
+	elseif (isset($headline)) {
 		if (isset($logo_image)) {
 			echo '<div id="HLogo" class="w3-quarter w3-container w3-hide-medium w3-hide-small">
 					<p class="w3-center"><img src="' . $logo_image .'"></p>
 				</div>';
 			}
-	echo '<div id="HHeadline" class="w3-twothird w3-container w3-hide-medium w3-hide-small">
+		echo '<div id="HHeadline" class="w3-twothird w3-container w3-hide-medium w3-hide-small">
 			<p class="w3-center">'. $headline .'</p>
 		</div>';
 	}
